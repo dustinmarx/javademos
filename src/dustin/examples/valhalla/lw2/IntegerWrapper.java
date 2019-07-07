513560efb9a9357848580a9aadd598f1e36be8d0
Dustin <dustinmarx@gmail.com>
2019-07-06 19:56:23 -0600

    Custom Integer-wrapping class
    Used to compare inline type wrapping an integer to a custom class wrapping an integer.

---
+++ src/dustin/examples/valhalla/lw2/IntegerWrapper.java
@@ -0,0 +1,14 @@
+package dustin.examples.valhalla.lw2;
+
+/**
+ * Simple custom wrapper for {@link Integer}.
+ */
+public class IntegerWrapper
+{
+   private int someIntegerValue;
+
+   public IntegerWrapper(final int newIntegerValue)
+   {
+      someIntegerValue = newIntegerValue;
+   }
+}
