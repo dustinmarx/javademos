6eaaad9375fde919203b3235f6733a262218f0fb
Dustin <dustinmarx@gmail.com>
2019-07-06 20:24:36 -0600

    New Package documentation for Valhalla LW2 demos
    Documents the package in which the classes that demonstrate Project Valhalla LW2 prototype concepts.

---
+++ src/dustin/examples/valhalla/lw2/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains code that demonstrates concepts
+ * from Project Valhalla's LW2 prototype.
+ *
+ * This code has been written against, built against, and
+ * executed against the Valhalla Early Access Build
+ * <b>Build jdk-14-valhalla+1-8 (2019/7/4)</b>
+ * downloaded from https://jdk.java.net/valhalla/.
+ */
+package dustin.examples.valhalla.lw2;
