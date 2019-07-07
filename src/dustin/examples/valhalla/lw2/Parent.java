1113a92a7390d0643890416bb4df81d064b91e47
Dustin <dustinmarx@gmail.com>
2019-07-06 19:55:25 -0600

    Initial no-op Parent class.
    This class is only present to attempt compilation that indicates that Valhalla LW2 inline types cannot extend classes.

---
+++ src/dustin/examples/valhalla/lw2/Parent.java
@@ -0,0 +1,10 @@
+package dustin.examples.valhalla.lw2;
+
+/**
+ * Arbitrary no-op class intended to be used to
+ * demonstrate that inline classes cannot extend
+ * other classes.
+ */
+public class Parent
+{
+}
