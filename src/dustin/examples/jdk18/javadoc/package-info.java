f364526afddd4becbe653833f3beea31a0f2cf72
Dustin <dustinmarx@gmail.com>
2021-09-25 18:32:55 -0600

    Create package-info.java

---
+++ src/dustin/examples/jdk18/javadoc/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Examples and demonstrations of Javadoc-related capabilities.
+ *
+ * The main entrypoint for the snippets example is the {@link SnippetExample} class:
+ * {@snippet :
+ *    final SnippetExample instance = SnippetExample.newInstance();  // @highlight substring="SnippetExample" @link substring="SnippetExample.newInstance()" target="SnippetExample#newInstance()"
+ * }
+ */
+package dustin.examples.jdk18.javadoc;
